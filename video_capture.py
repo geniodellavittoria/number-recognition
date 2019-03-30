40d373db840fac6fc41241c1722d44becdf2f12e
iabolzer <tim.bolzern@stud.hslu.ch>
2019-03-30 11:18:26 +0100

    changed some paramaters in capturing

--- video_capture.py
+++ video_capture.py
@@ -3,11 +3,15 @@ from time import sleep
 
 camera = PiCamera()
 
-
 def main():
+    camera.rotation = 180
+    camera.resolution = (720, 480)
+    camera.framerate = 90
     camera.start_preview()
     camera.start_recording('/home/pi/video.h264')
-    sleep(10)
+    for i in range(5):
+        print(i)
+        sleep(1)
     camera.stop_recording()
     camera.stop_preview()
 
