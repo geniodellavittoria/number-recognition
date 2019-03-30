189644b8a9746d459d6cde22cb5413908bfbb531
Tim Bolzern <t.bolzern@gmail.com>
2019-03-30 10:14:46 +0100

    added red and green contours

--- image_recognition.py
+++ image_recognition.py
@@ -9,26 +9,11 @@ import imutils
 from imutils import contours
 from imutils.video import VideoStream
 
-croppedImages = []
-
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--picamera", type=int, default=-1,
                 help="whether or not the Raspberry Pi camera should be used")
 args = vars(ap.parse_args())
-
-
-def save(img):
-    current = str(time.time())
-    name = 'Frames/frame_' + current + '.png'
-    cv.imwrite(name, img)
-    return name
-
-
-def crop(x, y, w, h, img):
-    # crop the image using array slices -- it's a NumPy array
-    cropped = img[y:y+h, x:x+w]
-    croppedImages.append(cropped)
 
 
 def detectShape(cont, orig):
@@ -44,32 +29,23 @@ def detectShape(cont, orig):
         # equal to one, otherwise, the shape is a rectangle
         shape = "square" if ar >= 0.95 and ar <= 1.15 else "rectangle"
         if shape == "square":
-            subimg = orig[y: y + h, x: x + w, :]
-            cv.imshow("subImg", subimg)
-            cv.imwrite("subimg.png", subimg)
-            return subimg
-        log.debug(ar)
-        return None
+            cv.drawContours(orig, [cont], -1, (0, 255, 0), 2)
+    else:
+        cv.drawContours(orig, [cont], -1, (0, 0, 255), 2)
+    return orig
 
 
-def findContours(edged, orig):
-    cv.threshold(edged, 60, 255, cv.THRESH_BINARY)[1]
-    edged = cv.GaussianBlur(edged, (5, 5), 0)
+def findContours(frame):
+    frame_resized = imutils.resize(frame.copy(), width=500)
+    edged = cv.Canny(frame_resized, 100, 200)
+    threshd = cv.threshold(edged, 60, 255, cv.THRESH_BINARY)[1]
+    edged = cv.GaussianBlur(threshd, (5, 5), 0)
     cnts = cv.findContours(edged, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     for c in cnts:
         # detect the name of the shape
-        detectShape(c, orig)
-        cv.drawContours(orig, [c], -1, (0, 255, 0), 2)
-
-
-def processImage(orig):
-    # prepare(src,img)
-    # findContours(img,orig)
-    for img in croppedImages:
-        print("cropped")
-        # predict(img)
-      # predict(img)
+        frame_resized = detectShape(c, frame_resized)
+    return frame_resized
 
 
 def img():
@@ -79,23 +55,16 @@ def img():
         log.error("img is null")
     log.debug("img loaded succesfully")
 
-    log.debug("start canny")
-    log.debug(image.shape)
-    resized = imutils.resize(image, width=300)
-    edged = imutils.auto_canny(resized)
-
-    log.debug("show canny")
-    cv.imshow("sss", edged)
-
     log.debug("start contour detection")
-    findContours(edged, resized)
+    image = findContours(image)
+    cv.imshow("frame", image)
     log.debug("finished contour detection")
 
     log.debug("wait until key is pressed")
     cv.waitKey(0)
 
 
-def pycam():
+def cam():
     # loop over the frames from the video stream
     # initialize the video stream and allow the cammera sensor to warmup
 
@@ -106,7 +75,10 @@ def pycam():
             # readimage
             frame = cap.read()[1]
             frame = imutils.resize(frame, width=300)
-            cv.imshow("Window", frame)
+            if frame is None:
+                log.error("no frame")
+            image = findContours(frame.copy())
+            cv.imshow("el Image", image)
             key = cv.waitKey(1) & 0xFF
 
             # if the `q` key was pressed, break from the loop
@@ -115,9 +87,6 @@ def pycam():
                 # do a bit of cleanup
                 cap.release()
                 cv.destroyAllWindows()
-            elif key == ord("c"):
-                processImage(frame)
-                pass
         except KeyboardInterrupt:
             # do a bit of cleanup
             cap.release()
@@ -127,8 +96,8 @@ def pycam():
 def main():
     log.basicConfig(level=log.DEBUG)
     log.info("program started")
-    # pycam()
-    img()
+    cam()
+    # img()
 
 
 if __name__ == "__main__":
--- number_prediction.py
+++ number_prediction.py
@@ -4,33 +4,34 @@ from keras.models import load_model
 model = load_model(r'.\\mnist_trained_model.h5')
 
 
-def getImg():
-    # getimgfromSource
-    print("not Implmented yet")
-
-
-def init(logger):
-    log = logger
+def init():
+    img = cv.imread("subimg.png", cv.COLOR_BGR2GRAY)
+    img.shape
+    start(img)
 
 
 def start(img):
     #cv.imshow("Window", im_bw)
     # resize using opencv
-    img_resized = cv.resize(img, (28, 28))
-    ############################################################
-    # invert image
-    im_gray_invert = 255 - img_resized
-    #cv.imshow("Window", im_gray_invert)
-    ####################################
-    print(im_gray_invert.shape)
-    im_final = im_gray_invert.reshape(1, 28, 28, 1)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    (thresh, img) = cv.threshold(img_gray,
+                                 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    sub_bw = cv.threshold(img, thresh, 255, cv.THRESH_BINARY_INV)[1]
+
+    sliced = img[:, :, 0]
+    img_resized = cv.resize(sliced, (28, 28))
+
     # the below output is a array of possibility of respective digit
+    im_final = sub_bw.reshape(1, 28, 28, 1)
+
     ans = model.predict(im_final)
     print(ans)
     # choose the digit with greatest possibility as predicted dight
     ans = ans[0].tolist().index(max(ans[0].tolist()))
     print('DNN predicted digit is: ', ans)
 
+    # cv.waitKey(0)
+
 
 if __name__ == "__main__":
-    getImg()
+    init()
