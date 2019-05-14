414c538c861466c4c827f5af0b14d5ed8a2389f6
Tim Bolzern <t.bolzern@gmail.com>
2019-05-14 18:21:18 +0200

    b

--- web_stream.py
+++ web_stream.py
@@ -82,8 +82,10 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     daemon_threads = True
 
 
-with picamera.PiCamera(resolution='640x480', framerate=40,iso=800) as camera:
+with picamera.PiCamera(resolution='640x480', framerate=40) as camera:
     log.info("start streaming camera")
+    camera.iso = 800
+    camera.shutter_speed = 500
     output = StreamingOutput()
     camera.start_recording(output, format='mjpeg')
     try:
